410faa00fe270ed5b348ebfe4997202d98aae389
vaibhav <vaibhavverma15062001@gmail.com>
2022-06-13 11:20:47 +0530

    portfolio

---
+++ db/db.js
@@ -0,0 +1,268 @@
+export default {
+  bio: {
+    about: {
+      text: [
+        "Hi &#128075;",
+        "I'm Vaibhav Kumar Verma. I am 3rd year undergrad in Electrical from NIT Jalandhar",
+        "I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+        "I love to read, listen to music, and travel.",
+      ],
+    },
+    contact: {
+      text: [
+        "If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+      ],
+    },
+  },
+  skills: [
+    {
+      title: "Languages",
+      skillName: "JavaScript, C++",
+      color: "1",
+      percentage: "90",
+    },
+    {
+      title: "Frameworks/Libraries",
+      skillName: "Reactjs",
+      color: "6",
+      percentage: "70",
+    },
+    {
+      title: "Backend",
+      skillName: "Nodejs, MongoDB",
+      color: "2",
+      percentage: "60",
+    },
+    {
+      title: "Design",
+      skillName: "HTML, Bootstrap,TailwindCSS",
+      color: "4",
+      percentage: "70",
+    },
+    {
+      title: "Version Control",
+      skillName: "GitHub",
+      color: "5",
+      percentage: "70",
+    },
+    {
+      title: "Tools",
+      skillName: "Postman, Chrome DevTools",
+      color: "3",
+      percentage: "80",
+    },
+    {
+      title: "Editor",
+      skillName: "VS Code,Sublime",
+      color: "6",
+      percentage: "90",
+    },
+  ],
+  projects: {
+    web: [
+      {
+        projectName: "Guess a number",
+        image: "",
+        summary:
+          "Developed a game using simple Javascript.If the number is less than the written number it shows less.If the the number is more than the written number is shows high and if it is same then it shows correct number.",
+        preview: "https://vaibhavkverma.github.io/Guess-a-number/",
+        techStack: ["JavaScript", "HTML", "CSS"],
+      },
+      {
+        projectName: "Dual Game",
+        image: "",
+        summary: "Developed a game using simple Javascript.",
+        preview: "https://vaibhavkverma.github.io/Dual-Game/",
+        techStack: ["HTML", "CSS", "JavaScript"],
+      },
+      {
+        projectName: "Flash Card App",
+        image: "",
+        summary: "A full object oriented programming based Flash Card App",
+        preview: "https://vaibhavkverma.github.io/JS-OOPS-Flashcard/",
+        techStack: ["HTML", "CSS", "JavaScript"],
+      },
+      {
+        projectName: "Budget App",
+        image: "",
+        summary: "A full object oriented programming based Budget App",
+        preview: "https://vaibhavkverma.github.io/JS-OOPS-BUDGETAPP/",
+        techStack: ["HTML", "CSS", "JavaScript"],
+      },
+      {
+        projectName: "Weather App",
+        image: "",
+        summary:
+          "Weather API used to show the temperature,humidity,precipitation,wind speed  for 1 week",
+        preview: "https://github.com/VaibhavKVerma/Weather-API",
+        techStack: ["HTML", "CSS", "JavaScript", "API", "AXIOS"],
+      },
+    ],
+    software: [
+      {
+        projectName: "Tours Api",
+        image: "",
+        summary:
+          "Created a tours api where there are tours,users and reviews collection.All are editable and there is admin,tour-guide and normal user",
+        preview: "https://github.com/VaibhavKVerma/Natours",
+        techStack: [
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "API",
+          "AXIOS",
+          "NodeJS",
+          "ExpressJS",
+          "PUG",
+        ],
+      },
+    ],
+    android: [
+      {
+        projectName: "Codeforces Progress Tracker App",
+        image: "",
+        summary:
+          "Codeforces API used to find detailed analysis of the user. Moreover a user can compare his profile with other user.Used ChartJS to show detailed analysis.",
+        preview: "https://codeforcesprogresstracker.netlify.app/",
+        techStack: ["ReactJS", "CSS", "JavaScript", "API", "AXIOS", "Netlify"],
+      },
+      {
+        projectName: "Anime Series Track App",
+        image: "",
+        summary:
+          "JIKAN Api used to get the data of all the anime.Used semantic ui for the styling.",
+        preview: "https://myanimeworld.netlify.app/",
+        techStack: [
+          "ReactJS",
+          "SemanticUI",
+          "JavaScript",
+          "API",
+          "AXIOS",
+          "Netlify",
+        ],
+      },
+      {
+        projectName: "Memory Game",
+        image: "",
+        summary: "A simple react game using functional components.",
+        preview: "https://memorygamereact.netlify.app/",
+        techStack: ["ReactJS", "CSS", "JavaScript", "Netlify"],
+      },
+      {
+        projectName: "Shopping App",
+        image: "",
+        summary:
+          "A react-redux based app where all the datas initially are stored in redux store and each item is also stored in redux store and displayed.",
+        preview: "https://github.com/VaibhavKVerma/reduxshoppingapp",
+        techStack: ["ReactJS", "CSS", "JavaScript", "Netlify"],
+      },
+    ],
+    freelance: [],
+  },
+  experience: [
+    {
+      title: "iMocha",
+      duration: "May 2022 - Present",
+      subtitle: "Problem Setter",
+      details: [
+        "Working as a problem setter for the imocha platform",
+        "Created 11 data structure and algorithms problems till now",
+      ],
+      tags: ["C++", "Data Strucutres", "Algorithms"],
+      icon: "heartbeat",
+    },
+    {
+      title: "Aim2Crack",
+      duration: "June 2021 - Dec 2021",
+      subtitle: "Frontend Lead Developer",
+      details: [
+        "Implemented Quiz Platform using the help of the API from the backend team.Implemented timer,sections,MCQs,Numerical and Multi Correct Questions.",
+        "Integrated ChartJS to display all the responses in a graph.",
+        "Implemented Student Panel where student can see correct/incorrect/missed problems",
+        "Implemented Teacher Panel where teacher can see problems,update the problems and the timer",
+        "Also implemented all the frontend javascript required to make website good.",
+      ],
+      tags: ["HTML", "CSS", "JavaScript", "ChartJS", "Bootstrap"],
+      icon: "qrcode",
+    },
+  ],
+  education: [
+    {
+      title: "Bachelors in Electrical Engineering",
+      duration: "",
+      subtitle: "National Institute of Technology, Jalandhar",
+      details: [
+        "Competitive Programmer with Codeforces Rating of 1368(PUPIL) and CodeChef Rating of 1809(4⭐).",
+        "Solved more than 850 problems on Leetcode and has a rank 7500.",
+      ],
+      tags: [
+        "C++",
+        "Data Structures & Algorithms",
+        "Web Development",
+        "Operating Systems",
+        "Database Management System",
+      ],
+      icon: "graduation-cap",
+    },
+    {
+      title: "Class 11-12th in Science and Mathematics",
+      duration: "",
+      subtitle: "Central Board of Secondary Education",
+      details: [
+        "Qualified JEE Advanced, Main & BITSAT.",
+        "Secured 85 percentage in Class 12th Boards Examinations.",
+      ],
+      tags: ["Physics", "Chemistry", "Mathematics"],
+      icon: "book",
+    },
+  ],
+  footer: [
+    {
+      label: "Dev Profiles",
+      data: [
+        {
+          text: "Codeforces",
+          link: "http://codeforces.com/profile/vaibhav_verma",
+        },
+        {
+          text: "GitHub",
+          link: "https://github.com/VaibhavKVerma",
+        },
+        {
+          text: "LeetCode",
+          link: "https://leetcode.com/forsakencode",
+        },
+      ],
+    },
+    {
+      label: "Resources",
+      data: [
+        {
+          text: "Enable Dark/Light Mode",
+          func: "enableDarkMode()",
+        },
+        {
+          text: "Print this page",
+          func: "window.print()",
+        },
+      ],
+    },
+    {
+      label: "Social Profiles",
+      data: [
+        {
+          text: "Linkedin",
+          link: "https://www.linkedin.com/in/vaibhav-kr-verma/",
+        },
+        {
+          text: "Instagram",
+          link: "https://www.instagram.com/web__ohhh/",
+        },
+      ],
+    },
+    {
+      label: "copyright-text",
+      data: ["Made with &hearts; by Vaibhav Kumar Verma."],
+    },
+  ],
+};
